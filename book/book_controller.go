fc07a2a25b29f51dd24088a476fd2fcedf0f6930
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2024-10-24 12:33:17 +0500

    book: get by id, update, delete

--- book/book_controller.go
+++ book/book_controller.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 type Controller struct {
@@ -16,6 +17,7 @@ func Router(router *mux.Router) {
 	r := router.PathPrefix("/book").Subrouter()
 	r.HandleFunc("", Controller{}.post)
 	r.HandleFunc("/all", Controller{}.getAll)
+	r.HandleFunc("/{id}", Controller{}.bookId)
 }
 
 func (c Controller) post(w http.ResponseWriter, r *http.Request) {
@@ -54,6 +56,51 @@ func (c Controller) getAll(w http.ResponseWriter, r *http.Request) {
 	err = json.NewEncoder(w).Encode(books)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error encoding books: %s", err.Error()), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (c Controller) bookId(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error parsing book id: %s", err.Error()), http.StatusBadRequest)
+		return
+	}
+
+	if r.Method == "GET" {
+		book, err := c.service.getById(id)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error getting book: %s", err.Error()), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(book)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error encoding book: %s", err.Error()), http.StatusInternalServerError)
+			return
+		}
+	} else if r.Method == "PUT" {
+		var book Book
+		err := utils.ParseRequest(r, &book)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error parsing request: %s", err.Error()), http.StatusBadRequest)
+			return
+		}
+
+		err = c.service.update(id, book)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error updating book: %s", err.Error()), http.StatusInternalServerError)
+			return
+		}
+	} else if r.Method == "DELETE" {
+		err := c.service.delete(id)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error deleting book: %s", err.Error()), http.StatusInternalServerError)
+			return
+		}
+	} else {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
 	}
 }
--- book/book_service.go
+++ book/book_service.go
@@ -1,6 +1,7 @@
 package book
 
 import (
+	"errors"
 	"example.com/library/author"
 	"example.com/library/utils"
 	"fmt"
@@ -44,4 +45,43 @@ func (s Service) getAll() ([]Book, error) {
 	}
 
 	return books, nil
+}
+
+func (s Service) getById(id int) (Book, error) {
+	query := fmt.Sprintf("select * from book where id=%d", id)
+	rows, err := utils.ExecuteSql(query)
+	if err != nil {
+		return Book{}, err
+	}
+
+	if !rows.Next() {
+		return Book{}, errors.New("book not found")
+	}
+
+	var book Book
+	err = rows.Scan(&book.Id, &book.Name, &book.Year, &book.AuthorId)
+	return book, err
+}
+
+func (s Service) update(id int, book Book) error {
+	_, err := s.authorService.GetById(book.AuthorId)
+	if err != nil {
+		return err
+	}
+
+	query := fmt.Sprintf(
+		"update book set name='%s', year=%d, author_id=%d where id=%d",
+		book.Name,
+		book.Year,
+		book.AuthorId,
+		id,
+	)
+	_, err = utils.ExecuteSql(query)
+	return err
+}
+
+func (s Service) delete(id int) error {
+	query := fmt.Sprintf("delete from book where id=%d", id)
+	_, err := utils.ExecuteSql(query)
+	return err
 }
